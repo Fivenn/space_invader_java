258bd2241d4876ed2f72b2ab0ee29b55d068c2cf
Nicolas SUEUR <nicolas1.sueur@orange.com>
2019-06-17 17:33:53 +0200

    update playgroundview

--- projet/src/projet/View/PlaygroundAreaView.java
+++ projet/src/projet/View/PlaygroundAreaView.java
@@ -1,12 +1,18 @@
 package projet.View;
 
+import projet.Controller.GameController;
 import projet.Model.gameClass.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
 
-public class PlaygroundAreaView extends JPanel {
-    public PlaygroundAreaView() {
+public class PlaygroundAreaView extends JPanel implements Observer {
+    private GameController gameController;
+    public PlaygroundAreaView(GameController gameController) {
+        this.gameController = gameController;
+        gameController.addObserver(this);
         setBackground(Color.BLACK);
 
         InformationGameAreaView informationGameAreaView = new InformationGameAreaView();
@@ -17,5 +23,11 @@ public class PlaygroundAreaView extends JPanel {
         this.add(informationGameAreaView, BorderLayout.NORTH);
 
         //Init tous les objets visuels
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
