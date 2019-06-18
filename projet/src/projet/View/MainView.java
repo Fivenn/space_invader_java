901838b209dc03223a05c43743ceda15eff6d7a7
MaelleH <maelle.98@live.fr>
2019-06-18 17:37:11 +0200

    CAZ MARCHE TA MERE

--- projet/src/projet/View/MainView.java
+++ projet/src/projet/View/MainView.java
@@ -9,13 +9,15 @@ import projet.Controller.GameController;
 import projet.Model.gameClass.Alien;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 /**
  *
  * @author mallou
  */
-public class MainView extends JFrame{
+public class MainView extends JFrame implements KeyListener{
     GameController gameController;
 
     public MainView(GameController gameController) throws HeadlessException {
@@ -36,7 +38,22 @@ public class MainView extends JFrame{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        this.addKeyListener(this);
         setVisible(true);
+        this.requestFocus();
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("projet.View.MainView.keyTyped()");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        this.gameController.actionJoueur(e.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
     
 }
