b450ef71ce7f884af7f4fda51dd5e9d6e3d1d99e
MaelleH <maelle.98@live.fr>
2019-06-23 15:15:55 +0200

    oui

--- projet/src/projet/View/GameOverView.java
+++ projet/src/projet/View/GameOverView.java
@@ -6,14 +6,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameOverView extends JPanel {
-
+    ImageIcon go = new ImageIcon(this.getClass().getClassLoader().getResource("gameOver.gif"));
     public GameOverView() {
         this.setBackground(Color.BLACK);
         this.setOpaque(true);
         this.setPreferredSize(new Dimension(900, 1000));
         this.setLayout(new BorderLayout());
 
-        JLabel gameOverLabel = new JLabel(new ImageIcon(this.getClass().getClassLoader().getResource("gameOver.gif")));
+        JLabel gameOverLabel = new JLabel(this.go);
         this.add(gameOverLabel, BorderLayout.CENTER);
     }
+
+    
+    
 }
